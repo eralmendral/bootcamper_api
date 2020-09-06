d59f0b9d3ab25918b3af2054c992b552f9d6c594
eralmendral <eralmendral@gmail.com>
2020-09-06 14:42:45 +0800

    Connected to mongodb cloud database and added model for bootcamps

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
 
+// configs
 const dotenv = require('dotenv');
 dotenv.config({ path: './config/config.env'})
+
+// Body parser
+app.use(express.json());
+
+// Database connection
+connectDB();
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, (req, res) => {
   console.log(`Env:`, process.env.NODE_ENV)
   console.log(`Server running at port: ${port}`)
+})
+
+// Handle errors
+process.on('unhandledRejection', (err, promise) => {
+  console.log('Error:', err.message)
+  // Close server and exit process
+  server.close(() => process.exit(1));
 })
---
+++ models/Bootcamp.js
@@ -0,0 +1,102 @@
+const mongoose = require('mongoose');
+
+const BootcampSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [ true , 'Please add a name' ],
+    unique: true,
+    trim: true,
+    maxlength: [ 50, 'Name can not be more than 50 characters']
+  },
+  slug: String,
+  description: {
+    type: string,
+    required: [ true, 'Please add a description'],
+    maxlength: [ 500, 'Description can not be more than 500 characters']
+  },
+  website: {
+    type: String,
+    match: [ /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+    , 'Please add a valid website url.']
+  },
+  phone: {
+    type: String,
+    maxlength: [20, 'Phone can not be more than 20 characters']
+  },
+  email: {
+    type: String,
+    match: [
+      /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
+      'Please add a valid email.'
+    ]
+  },
+  address: {
+    type: String,
+    required: [true, 'Please add an address']
+  },
+  location: {
+    // GeoJSON Point
+    type: {
+      type: String,
+      enum: [ Point ],
+      required: true
+    },
+    coordinates: {
+      type: [ Number ],
+      required: true,
+      index: '2dsphere'
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String
+  },
+  careers : {
+    // Array of strings
+    type: [ String ],
+    required: true,
+    enum: [
+      'Web Development',
+      'Mobile Development',
+      'UI/Ux',
+      'Data Science',
+      'Business',
+      'Machine Learning',
+      'Other'
+    ]
+  },
+  averageRating: {
+    type: Number,
+    min: [1, 'Rating must be at least 1'],
+    max: [10, 'Rating can not be more than 10']
+  },
+  averageCost: Number,
+  photo: {
+    type: String,
+    default: 'no-photo.jpg'
+  },
+  housing: {
+    type: Boolean,
+    default: false
+  },
+  jobAssistance: {
+    type: Boolean,
+    default: true
+  },
+  jobGuarantee: {
+    type: Boolean,
+    default: false
+  },
+  acceptGi : {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Bootcamp', BootcampSchema);
