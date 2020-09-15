cc67243ea658d13e8d13a17e64368dc89e4101a2
eralmendral <eralmendral@gmail.com>
2020-09-15 23:01:49 +0800

    feat: get all / single bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,7 +1,12 @@
 const Bootcamp = require('../models/Bootcamp')
 
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+    res.status(200).json({ success: true, data: bootcamps });
+  } catch(err) {
+    res.status(400).json({ success: false })
+  }
 }
 
 exports.createBootcamp = async (req, res, next) => {
@@ -9,14 +14,21 @@ exports.createBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.create(req.body);
     res.status(201).json({ success: true, data: bootcamp })
   } catch (err) {
-    console.log('error:', err);
     res.status(400).json({ success: false })
   }
-
 }
 
-exports.getBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: `Show bootcamp with ID: ${req.params.id}` });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+     const bootcamp = await Bootcamp.findById(req.params.id);
+     if(!bootcamp) {
+       return  res.status(400).json({ success: false })
+     }
+
+     res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false })
+  }
 }
 
 exports.updateBootcamp = (req, res, next) => {
