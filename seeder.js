a10838b8cf06087058dc39459ad83206dc5bb86d
eralmendral <eralmendral@gmail.com>
2020-09-20 18:17:34 +0800

    added data import and delete

---
+++ seeder.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+// Load env variables
+dotenv.config({ path: './config/config.env' });
+
+// Load models
+const Bootcamp = require('./models/Bootcamp');
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: true,
+  useUnifiedTopology: true
+});
+
+// Read json files
+const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf8'));
+
+// Import data to Database
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+
+    console.log('Data imported...'.green.inverse);
+    process.exit();
+
+  } catch (error) {
+    console.error('Error importing data...', error);
+  }
+}
+
+// Delete data from Database
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+
+    console.log('Data deleted...'.red.inverse);
+    process.exit();
+
+  } catch (error) {
+    console.error('Error deleting data...'.red);
+  }
+}
+
+
+// Argument to check if argument if import/delete
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
