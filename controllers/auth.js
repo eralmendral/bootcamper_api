5c05c6226c0b154d5673fe652ca94330299fc2ab
eralmendral <eralmendral@gmail.com>
2020-10-17 14:00:23 +0800

    encript password using bcrypt and sign jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,17 @@ const User = require('../models/User')
 // @access public
 
 exports.register = asyncHandler(async(req, res, next) => {
+  const { name, email, password, role } = req.body;
+   
+  // Create user
+  const user = await User.create({
+    name, email, password, role
+  })
+
+  // Create token 
+  const token = user.getSignedJwtToken();
+
   res.status(200).json({
-    success: true
+    success: true, token
   })
 }) 
--- models/User.js
+++ models/User.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = mongoose.Schema({
   name: {
     type: String,
-    require: true
+    required: true
   },
   email: {
     type: String,
-    require: true,
+    required: true,
     unique: true,
     match: [
       /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
@@ -32,5 +34,18 @@ const UserSchema = mongoose.Schema({
     default: Date.now
   }
 })
+
+// Encrypt password using bcrypt
+UserSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+})
+
+// Sign JWT and return 
+UserSchema.methods.getSignedJwtToken = function() {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE
+  })
+}
 
 module.exports = mongoose.model('User', UserSchema);
