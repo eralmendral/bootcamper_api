8df84327363bc902ec3735ba9049b753786f465c
eralmendral <eralmendral@gmail.com>
2020-10-17 15:54:56 +0800

    feat: protect route action via use roles

---
+++ middleware/auth.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('./asyncHandler');
+const ErrorResponse = require('../utils/ErrorResponse');
+const User = require('../models/User');
+
+// Protect routes
+exports.protect = asyncHandler(async (req, res, next) => {
+  let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(" ")[1]; // dont include 'Bearer' string
+  }
+
+  // Make sure token exists
+  if (!token) {
+    return next(new ErrorResponse('Not authorized to access this route.', 401));
+  }
+
+  // Verify token 
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id);  // find user using user id from token payload and attach to request
+    next();
+  } catch (error) {
+    return next(new ErrorResponse('Not authorized to access this route.', 401));
+  }
+})
+
+// Grant access to specific roles
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if(!roles.includes(req.user.role)) {
+      return next(new ErrorResponse(`User role ${req.user.role} is not authorized to access this route.`, 403));
+    }
+    next();
+  }
+}
