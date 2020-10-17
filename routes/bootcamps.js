1718cf1c692a20575808a382133c3ee8252818a6
eralmendral <eralmendral@gmail.com>
2020-10-17 15:18:45 +0800

    send jwt in a cookie

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { getBootcamps, createBootcamp, getBootcamp, updateBootcamp, deleteBootcamp, getBootcampInRadius, bootcampPhotoUpload } = require('../controllers/bootcamps');
 
+const { protect } = require('../middleware/auth');
+
 const advanceResults = require('../middleware/advanceResults');
 const Bootcamp = require('../models/Bootcamp');
 
@@ -10,8 +12,8 @@ const courseRouter = require('./courses');
 router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampInRadius);
-router.route('/').get(advanceResults(Bootcamp, 'courses'), getBootcamps).post(createBootcamp);
-router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
+router.route('/').get(advanceResults(Bootcamp, 'courses'), getBootcamps).post(protect, createBootcamp);
+router.route('/:id').get(getBootcamp).put(protect, updateBootcamp).delete(protect, deleteBootcamp);
 router.route('/:id/photo').put(bootcampPhotoUpload);
 
 module.exports = router;
--- routes/courses.js
+++ routes/courses.js
@@ -3,13 +3,15 @@ const { getCourses, getCourse, addCourse, updateCourse, deleteCourse } = require
 const Course = require('../models/Course');
 const advanceResults = require('../middleware/advanceResults');
 
+const { protect } = require('../middleware/auth');
+
 const router = express.Router({ mergeParams: true });
 
 router.route('/').get(advanceResults(Course, {
   path: 'Bootcamp',
   select: 'name description'
-}), getCourses).post(addCourse);
+}), getCourses).post(protect, addCourse);
 
-router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
+router.route('/:id').get(getCourse).put(protect, updateCourse).delete(protect, deleteCourse);
 
 module.exports = router;
